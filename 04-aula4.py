67a5907af7e86c74766a2792c3399d73faa019b0
Sávio Luís <savio9barros@gmail.com>
2024-09-15 13:43:10 -0300

    Create 04-aula4.py

---
+++ 04-aula4.py
@@ -0,0 +1,14 @@
+nome = "Sávio e Guilherme"
+idade = 17 
+altura  = 1.75
+
+
+# exibir mensagem na tela
+print(" nome: ",nome, "idade: ",idade, "altura:, ", altura )
+# formulário com f-string
+print(f"nome:  {nome} idade: {idade} altura: {altura}")
+
+nome = input("Digite o seu none: ")
+idade = input("Digite a sua idade: ")
+altura = float(input("Digite a sua altura"))
+print(f"nome:  {nome} idade: {idade} altura: {altura}")
