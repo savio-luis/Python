434bfea1abeb93ac175398752bdeb73afcfeab08
Sávio Luís <savio9barros@gmail.com>
2024-09-15 13:41:52 -0300

    03.aula3.py

---
+++ 03.aula3.py
@@ -0,0 +1,16 @@
+# Variáveis em Python
+# Não há necessidade de declará-las no ínicio do programa
+# Basta criar no mais do algoritmo com a atribuição
+
+teste = "Sávio"
+print(teste) # Imprime a string "Eduardo"
+print(type(teste)) # Imprime o tipo da variável, string
+teste = 10
+print(teste) # Imprime o número 10
+print(type(teste)) # Imprime o tipo da variável, int
+teste = 1.74
+print(teste) # Imprime o número (1, 74)
+print(type(teste)) # Imprime o tipo da variável, que é float
+teste = True
+print(teste) # Imprime a string True
+print(type(teste)) # Imprime o tipo da variável, que é bool
