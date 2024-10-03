714c440665f72a296379eedadd6aadbabade4f9e
Sávio Luís <savio9barros@gmail.com>
2024-10-03 10:56:10 -0300

    Create 16-funçãosoma.py

---
+++ 16-funçãosoma.py
@@ -0,0 +1,7 @@
+def soma(n1, n2):
+    return n1 + n2
+
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+s = soma(n1, n2)
+print('A soma de {} e {} é {}!'.format(n1, n2, s))
