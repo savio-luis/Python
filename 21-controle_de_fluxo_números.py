04f17390a549ce94f2690b16c915dea8ad28e17d
Sávio Luís <savio9barros@gmail.com>
2024-10-17 11:39:17 -0300

    Add files via upload

---
+++ 21-controle_de_fluxo_números.py
@@ -0,0 +1,7 @@
+for numero in range(1, 100):
+    if numero == 6 or (numero >= 10 and numero <= 27):
+        print(f"Não vou mostrar o {numero}.")
+    else:
+        print(numero)
+
+print("Acabou!")
